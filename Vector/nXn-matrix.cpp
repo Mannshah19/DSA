8883dae0eeed911ccc7368971a97c867e79a83d2
Mann Shah <mannshah4798@gmail.com>
2026-04-09 20:52:44 +0530

    new file for n cross n matrix is added in vector folder

---
+++ Vector/nXn-matrix.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    int n;
+
+    cout << "Enter size of matrix: ";
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Enter element at [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
+
+
+
+}
