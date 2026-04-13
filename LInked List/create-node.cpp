ba16ad7b76b1398b33301a453c0c006005e1b266
Mann Shah <mannshah4798@gmail.com>
2026-04-13 22:26:14 +0530

    new topic about node has been started

---
+++ LInked List/create-node.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+class Node
+    
+    {
+
+    public:
+
+
+
+    int data;
+
+    Node *next;
+
+
+
+
+
+    };
+
+
+int main()
+
+{
+
+    Node *HEAD = NULL;
+
+    HEAD = new Node();
+
+    HEAD -> data = 10;
+    HEAD -> next = NULL;
+    
+    cout << "First Node Address: " << HEAD << endl;
+    cout << "First Node Data: " << HEAD -> data << endl;
+    cout << "First Node next: " << HEAD -> next << endl;
+    cout << endl;
+
+    
+    Node *n2 = NULL;
+    n2 = new Node();
+
+    n2 -> data = 20;
+    n2 -> next = NULL;
+    HEAD -> next = n2;
+
+    
+    
+    cout << "Second Node Address: " << n2 << endl;
+    cout << "Second Node Data: " << n2 -> data << endl;
+    cout << "Second Node next" << n2 -> next << endl;
+    cout << "First Node next: " << HEAD -> next << endl;
+    cout << endl;
+
+
+    
+
+
+
+
+
+
+    return 0;
+
+
+
+}
