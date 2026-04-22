69b03f4d1d1998738c06536689e37c7e7df38eb7
Mann Shah <mannshah4798@gmail.com>
2026-04-22 21:01:20 +0530

    Implement basic stack operations menu in stack-operations.cpp

---
+++ Stack/stack-operations.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+
+
+int main ()
+{
+
+    int choice;
+
+    cout << "Enter your choice : " << endl;
+    cin >> choice;
+
+    cout << "1. Push in Stack....." << endl;
+    cout << "2. Pop from Stack....." << endl;
+    cout << "3. Peek of Stack....." << endl;
+    cout << "4. View  Stack....." << endl;
+    cout << "5. Check empty or not....." << endl;
+    cout << "6. Check full or not....." << endl;
+    cout << "7. Check size of stack....." << endl;
+    cout << "0. Exit....." << endl;
+
+    do
+    {
+
+
+        switch (choice)
+        {
+        case 1:
+        {
+
+            
+            
+            break;
+        }
+
+
+        case 2:
+        {
+
+            
+            
+            break;
+        }
+
+        case 3:
+        {
+
+            
+            
+            break;
+        }
+
+        case 4:
+        {
+
+            
+            
+            break;
+        }
+
+        case 5:
+        {
+
+            
+            
+            break;
+        }
+
+        case 6:
+        {
+
+            
+            
+            break;
+        }
+
+        case 7:
+        {
+
+            
+            
+            break;
+        }
+
+
+        case 0:
+        {
+
+            
+            
+            break;
+        }
+
+
+        
+        default:
+            break;
+        }
+        
+    } while (choice);
+    
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
