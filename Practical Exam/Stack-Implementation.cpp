d2ac61698177c5c67bc2ad72904a4ef29e973174
Mann Shah <mannshah4798@gmail.com>
2026-05-04 10:09:02 +0530

    Implement stack data structure with push, pop, and view operations

---
+++ Practical Exam/Stack-Implementation.cpp
@@ -0,0 +1,298 @@
+#include<iostream>
+using namespace std;
+
+
+class Stack 
+{
+
+
+    public:
+
+
+        int *arr;
+
+        int top;
+
+        int size;
+
+        int count = 0 , ele;
+        
+        Stack(int s) {
+
+            size = s;
+
+            arr = new int[size];
+
+            top = -1;
+
+            count = 0;
+
+        }
+
+
+
+        void pushStack()
+        {
+
+            if (top >= size - 1)
+            {
+
+                cout << "Stack overflow";
+            }
+            
+
+            else
+            {
+
+
+                cout << "Enter element to push : ";
+                cin >> ele;
+
+                top++;
+
+                arr[top] = ele;
+
+                count++;
+
+                cout << "Element pushed successfully" << endl;
+
+            }
+            
+        }
+
+        void viewStack()
+        {
+
+            if (top == -1)
+            {
+
+                cout << "Stack is empty" << endl;
+            }
+            
+
+            else
+            {
+
+                cout << "Stack Elements : ";
+
+                for (int i = top; i >= 0 ; i--)
+                {
+
+                    cout << arr[i] << " ";
+
+                }
+
+                cout << endl;
+
+            }
+            
+        }
+
+        void popStack()
+        {
+
+            if (top == -1)
+            {
+
+                cout << "Stack underflow" << endl;
+            }
+            
+
+            else
+            {
+
+                cout << "Popped element : " << arr[top] << endl;
+
+                top--;
+
+                count--;
+
+            }
+            
+        }
+
+
+    
+
+            while (decimal > 0)
+            {
+
+                int remainder = decimal % 2;
+
+                if (top >= size - 1)
+                {
+
+                    cout << "Stack overflow" << endl;
+                    return;
+
+                }
+
+                top++;
+
+                arr[top] = remainder;
+
+                count++;
+
+                decimal = decimal / 2;
+
+            }
+
+            cout << "Binary : ";
+
+            for (int i = top; i >= 0; i--)
+            {
+
+                cout << arr[i];
+
+            }
+
+            cout << endl;
+
+            top = -1;
+            count = 0;
+
+        }
+        
+        
+        ~Stack() 
+        {
+
+            delete[] arr;
+
+        }
+
+
+
+
+};
+
+
+int main ()
+{
+    int size;
+    cout << "Enter Size of a Stack : ";
+    cin >> size;
+    
+    Stack s(size);
+
+    int choice;
+
+    do
+
+    {
+
+        cout << "\n1. Push in Stack....." << endl;
+        cout << "2. Pop from Stack....." << endl;
+        cout << "4. View  Stack....." << endl;
+        cout << "0. Exit....." << endl;
+
+
+        cout << "Enter your choice : ";
+        cin >> choice;
+
+
+        switch (choice)
+        {
+        case 1:
+        {
+            s.pushStack();
+            
+            
+            break;
+        }
+
+
+        case 2:
+        {
+
+            s.popStack();
+            
+            
+            break;
+        }
+
+        case 3:
+        {
+
+            s.peekStack();
+            
+            
+            break;
+        }
+
+        case 4:
+        {
+
+            s.viewStack();
+            
+            
+            break;
+        }
+
+        case 5:
+        {
+
+            s.isEmpty();
+            
+            
+            break;
+        }
+
+        case 6:
+        {
+
+            s.isFull();
+            
+            
+            break;
+        }
+
+        case 7:
+        {
+
+            s.sizeStack();
+            
+            
+            break;
+        }
+
+        case 8:
+        {
+
+            s.decimalToBinary();
+            
+            
+            break;
+        }
+
+
+        case 0:
+        {
+
+            cout << "Exiting....." << endl;
+            
+            
+            break;
+        }
+
+
+        
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
+        }
+        
+    } while (choice != 0);
+    
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
